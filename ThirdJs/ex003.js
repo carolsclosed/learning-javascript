3756e67f743f9c508cf4e0befe5a7fe4cc40a951
Carolina Sebastião <108016647+carolsclosed@users.noreply.github.com>
2022-07-15 19:26:27 +0100

    update

--- ThirdJs/ex003.js
+++ ThirdJs/ex003.js
@@ -1,2 +1,5 @@
 var p1 = window.document.getElementsByTagName("p")[0];
 window.document.write("its written like this: " + p1.innerText);
+p1.style.color = "blue";
+var corpo = window.document.body;
+corpo.style.background = "yellow";
