dfa1c26d75d7345d298e2a84e7b1057d81546864
Carolina Sebastião <108016647+carolsclosed@users.noreply.github.com>
2022-07-15 19:42:07 +0100

    ended this part

--- ThirdJs/ex003.js
+++ ThirdJs/ex003.js
@@ -1,6 +1,10 @@
 var p1 = window.document.getElementsByTagName("p")[1];
 var corpo = window.document.body;
-var d = window.document.getElementById("msg");
-d.style.background = "green";
+//var d = window.document.getElementById("msg");
+//d.style.background = "green";
+//d.innerText = "Im waiting... ";
 p1.style.color = "blue";
 corpo.style.background = "yellow";
+
+var d = window.document.querySelector("div#msg");
+d.style.background = "red";
