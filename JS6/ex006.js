6e6569510344face7b12fd9c7dc6b52aeccc0c7f
Carolina Sebastião <108016647+carolsclosed@users.noreply.github.com>
2022-07-17 14:51:03 +0100

    ended the ex006

---
+++ JS6/ex006.js
@@ -0,0 +1,7 @@
+var vel = 10;
+console.log(` Your car's speed is ${vel}Km/h`);
+if (vel > 60) {
+  //simple condition
+  console.log("You have exceeded the speed limit.");
+}
+console.log("Always drive wearing a seat belt.");
