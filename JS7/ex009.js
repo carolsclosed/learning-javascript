05242eab60ba7360e351a7a84d39c4a78498fcb3
Carolina Sebastião <108016647+carolsclosed@users.noreply.github.com>
2022-07-17 16:00:46 +0100

    ended ex009

---
+++ JS7/ex009.js
@@ -0,0 +1,9 @@
+var age = 22;
+console.log(`You have ${age}.`);
+if (age < 16) {
+  console.log("don't vote.");
+} else if (age < 18) {
+  console.log("vote by choice.");
+} else {
+  console.log("vote.");
+}
