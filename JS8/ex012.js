32462018084eab36ea527388501cd8aa317047a7
Carolina Sebastião <108016647+carolsclosed@users.noreply.github.com>
2022-07-18 20:18:16 +0100

    ended the site

--- JS8/ex012.js
+++ JS8/ex012.js
@@ -1,1 +1,17 @@
-
+function charge() {
+  var msg = window.document.getElementById("msg");
+  var img = window.document.getElementById("img");
+  var date = new Date();
+  var hr = date.getHours();
+  msg.innerHTML = `It's exactly ${hr} hours.`;
+  if (hr >= 0 && hr < 12) {
+    img.src = "imgs/morning.png";
+    document.body.style.background = "#FFA07A";
+  } else if (hr >= 12 && hr < 18) {
+    img.src = "imgs/afternoon.png";
+    document.body.style.background = "#BA55D3";
+  } else {
+    img.src = "imgs/night.png";
+    document.body.style.background = "#191970";
+  }
+}
