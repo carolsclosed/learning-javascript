0194ec169d0354bde57756ff80ef070e9ead9fb3
Carolina Sebastião <108016647+carolsclosed@users.noreply.github.com>
2022-07-18 22:13:51 +0100

    ended the age checker

--- JS9/ex013.js
+++ JS9/ex013.js
@@ -3,11 +3,50 @@ function verify() {
   var year = date.getFullYear();
   var fyear = document.getElementById("txtyear");
   var res = document.getElementById("res");
-  if (fyear.value.length == 0 || fyear.value > year) {
+  if (fyear.value.length == 0 || Number(fyear.value) > year) {
     alert("[ERROR] Please check your data and try again");
   } else {
     var fgender = document.getElementsByName("radgender");
     var age = year - Number(fyear.value);
-    res.innerHTML = `Your have ${age} years old`;
+    var img = document.createElement("img");
+    img.setAttribute("id", "photo");
+    if (fgender[0].checked) {
+      gender = "Man";
+      if (age >= 0 && age < 10) {
+        //criança
+        img.setAttribute("src", "imgs/bebegaroto.jpg");
+      } else if (age >= 10 && age < 21) {
+        //Jovem
+        img.setAttribute("src", "imgs/jovemgaroto.jpg");
+      } else if (age >= 21 && age < 50) {
+        //adulto
+        img.setAttribute("src", "imgs/adultohomem.jpg");
+      } else if (age >= 50) {
+        //idoso
+        img.setAttribute("src", "imgs/idosohomem.jpg");
+      }
+    } else if (fgender[1].checked) {
+      gender = "Woman";
+      if (age >= 0 && age < 10) {
+        //criança
+        img.setAttribute("src", "imgs/bebegarota.jpg");
+      } else if (age >= 10 && age < 21) {
+        //Jovem
+        img.setAttribute("src", "imgs/jovemgarota.jpg");
+      } else if (age >= 21 && age < 50) {
+        //adulto
+        img.setAttribute("src", "imgs/adultamulher.jpg");
+      } else if (age >= 50) {
+        //idoso
+        img.setAttribute("src", "imgs/idosamulher.jpg");
+      }
+    }
   }
+
+  res.style.textAlign = "center";
+  res.innerHTML = `${gender} of ${age} years old`;
+  res.appendChild(img);
+  img.style.width = "250px";
+  img.style.height = "240px";
+  img.style.borderRadius = "50%";
 }
