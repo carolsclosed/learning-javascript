b36d46980784763320d1958e439afef7d123158d
Carolina Sebastião <108016647+carolsclosed@users.noreply.github.com>
2022-07-18 21:42:10 +0100

    update

--- JS9/ex013.js
+++ JS9/ex013.js
@@ -1,1 +1,13 @@
-function verify() {}
+function verify() {
+  var date = new Date();
+  var year = date.getFullYear();
+  var fyear = document.getElementById("txtyear");
+  var res = document.getElementById("res");
+  if (fyear.value.length == 0 || fyear.value > year) {
+    alert("[ERROR] Please check your data and try again");
+  } else {
+    var fgender = document.getElementsByName("radgender");
+    var age = year - Number(fyear.value);
+    res.innerHTML = `Your have ${age} years old`;
+  }
+}
