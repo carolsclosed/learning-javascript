3d648c537122d6bc731b96da8e0e3fc6f88148e7
Carolina Sebastião <108016647+carolsclosed@users.noreply.github.com>
2022-07-19 13:51:59 +0100

    repetitions

---
+++ JS10/ex014.js
@@ -0,0 +1,14 @@
+var c = 1;
+while (c <= 6) {
+  console.log("all fine");
+  c++; // c = c+1
+}
+
+/*
+console.log("all fine");
+console.log("all fine");
+console.log("all fine");
+console.log("all fine");
+console.log("all fine");
+console.log("all fine");
+*/
