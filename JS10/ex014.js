40e1cc318ee3303f7bb83729ceff01ce31ac74e6
Carolina Sebastião <108016647+carolsclosed@users.noreply.github.com>
2022-07-19 14:00:33 +0100

    update

--- JS10/ex014.js
+++ JS10/ex014.js
@@ -1,8 +1,16 @@
 var c = 1;
+do {
+  console.log(`step ${c}`);
+  c++; // c = c+1
+} while (c <= 6);
+
+/*
+var c = 1;
 while (c <= 3) {
   console.log(`step ${c}`);
   c++; // c = c+1
 }
+*/
 
 /*
 console.log("all fine");
